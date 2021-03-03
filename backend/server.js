8bf9cd00ca933fb873b02d8dd290a368f855b5fc
mks-94 <muralikrishna9424.com>
2021-03-03 06:56:58 +0530

    Implemented server connection and DB connection

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running on port:${PORT}`));
