e1e81e7fd5d7405cb8bfa452ca2bc7b890b9a294
mks-94 <muralikrishna9424.com>
2021-04-06 07:09:09 +0530

    Implemented backdrop and side drawer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 
 import Navbar from "./components/Navbar";
+import Backdrop from "./components/Backdrop";
+import SideDrawer from "./components/SideDrawer";
+
 import "./App.css";
 
 const App = () => {
+  const [sideToggle, setSideToggle] = useState(false);
+
   return (
     <Router>
-      <Navbar />
+      <Navbar click={() => setSideToggle(true)} />
+      <Backdrop show={sideToggle} click={() => setSideToggle(false)} />
+      <SideDrawer show={sideToggle} click={() => setSideToggle(false)} />
       <main>
         <Switch>
           <Route exact path="/" component={HomeScreen} />
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
-function Navbar() {
+function Navbar({ click }) {
   return (
     <nav className="navbar">
       <div className="navbar__logo">
@@ -23,7 +23,7 @@ function Navbar() {
         </li>
       </ul>
 
-      <div className="hamburger__menu">
+      <div className="hamburger__menu" onClick={click}>
         <div></div>
         <div></div>
         <div></div>
