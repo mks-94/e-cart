2232ba27545ab04e7f750b1a4a79cde74008dec1
mks-94 <muralikrishna9424.com>
2021-05-12 17:45:52 +0530

    Implemented Product view styles

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,21 @@
+import Product from "../components/Product";
 import "./HomeScreen.css";
 
 const HomeScreen = () => {
-  return <div className="homescreen">I'm from HomeScreen</div>;
+  return (
+    <div className="homescreen">
+      <h2 className="homescreen__title">Latest Products</h2>
+
+      <div className="homescreen__products">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product/>
+      </div>
+    </div>
+  );
 };
 
 export default HomeScreen;
